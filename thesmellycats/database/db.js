2b6df3b0f27de6cca21d0e7d0a16f87dce6cb5ed
Axel Wulff Sæther <axelwulff@mac.com>
2022-02-19 13:51:12 +0100

    Remover logon

--- thesmellycats/database/db.js
+++ thesmellycats/database/db.js
@@ -3,7 +3,7 @@ import mongoose from 'mongoose';
 export default async function connectToDb(){
     if(mongoose.connection.readyState >= 1) return;
 
-    const uri = `mongodb+srv://axelwulff:9IedCqR8tNbJdMFE@realmcluster.bh8xv.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`
+    const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@realmcluster.bh8xv.mongodb.net/${process.env.DB_NAME}?retryWrites=true&w=majority`
     return mongoose.connect(uri,{
         useNewUrlParser:true,
         useUnifiedTopology: true
